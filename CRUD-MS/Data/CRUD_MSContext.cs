686bb5db4642c216929eae89210ce506edad6156
2122944 <2122944@LTIN403211>
2023-04-13 16:52:24 +0530

    Add project files.

---
+++ CRUD-MS/Data/CRUD_MSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUD_MS.Model;
+
+namespace CRUD_MS.Data
+{
+    public class CRUD_MSContext : DbContext
+    {
+        public CRUD_MSContext (DbContextOptions<CRUD_MSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUD_MS.Model.Student> Student { get; set; } = default!;
+    }
+}
---
+++ CRUD-MS/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUD_MS.Data;
+using CRUD_MS.Model;
+
+namespace CRUD_MS.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CRUD_MS.Data.CRUD_MSContext _context;
+
+        public IndexModel(CRUD_MS.Data.CRUD_MSContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Student != null)
+            {
+                Student = await _context.Student.ToListAsync();
+            }
+        }
+    }
+}
